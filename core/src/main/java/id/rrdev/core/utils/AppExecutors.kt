ee2f32b5d36eb80a8261113878c6225f29a409d6
Rasyid <rasydridla298@gmail.com>
2021-11-06 15:21:26 +0700

    fixing code

--- core/src/main/java/id/rrdev/core/utils/AppExecutors.kt
+++ core/src/main/java/id/rrdev/core/utils/AppExecutors.kt
@@ -24,6 +24,8 @@ class AppExecutors @VisibleForTesting constructor(
     )
 
     fun diskIO(): Executor = diskIO
+    fun networkIO(): Executor = networkIO
+    fun mainThread(): Executor = mainThread
 
     private class MainThreadExecutor : Executor {
         private val mainThreadHandler = Handler(Looper.getMainLooper())
